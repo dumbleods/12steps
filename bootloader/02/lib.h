4b8f8c0e2f9d0bdf9e2cdd48dde5bf976cdfd5b5
dumbleods <dumbleods@excite.co.jp>
2016-07-03 00:25:31 +0900

    Changes to be committed: 	new file:   02/Makefile 	new file:   02/defines.h 	new file:   02/ld.scr 	new file:   02/lib.c 	new file:   02/lib.h 	new file:   02/main.c 	new file:   02/serial.c 	new file:   02/serial.h 	new file:   02/startup.s 	new file:   02/vector.c

---
+++ bootloader/02/lib.h
@@ -0,0 +1,15 @@
+#ifndef _LIB_H_INCLUDED_
+#define _LIB_H_INCLUDED_
+
+void *memset(void *b, int c, long len);
+void *memcpy(void *dst, const void *src, long len);
+int memcmp(const void *b1, const void *b2, long len);
+int strlen(const char *s);
+char *strcpy(char *dst, const char *src);
+int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, int len);
+int putc(unsigned char c); /* for character */
+int puts(unsignec har *str); /* for string */
+int putxval(unsigned long value, int column); /* output hex number */
+
+#endif
